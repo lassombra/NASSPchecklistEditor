47e34a65d6f3658de3a94e6a5c7d3c5d25269980
lassombra <briatt22@gmail.com>
2025-03-29 15:34:47 -0500

    Add ability to remove a tab by either detaching it or fully deleting it.

--- acdhandler.h
+++ acdhandler.h
@@ -2,6 +2,7 @@
 #define ACDHANDLER_H
 
 #include <QWidget>
+#include "removedialog.h"
 #include "tabpool.h"
 
 class ACDHandler : public QTabWidget
@@ -19,9 +20,14 @@ private:
     QStringList saveData(QStringList metadata, const QList<QStringList> &data);
     void saveData(QString filename, const QStringList &headers, const QList<QStringList> &data);
     QList<QStringList> parseFile(QString filename);
+    void initRemoveTab(int index);
+    void cancelRemove();
+    void acceptRemove(int decision);
 
     QFileInfo* currentFile;
     TabPool* tabPool;
+    RemoveDialog* removeDialog;
+    int m_pendingRemoveIndex;
 signals:
     void statusMessage(QString message, int timeout = 0);
     void fileLoadedChanged(bool loaded);
