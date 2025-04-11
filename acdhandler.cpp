6d6e0afafbe1cf37e735d121cd987e4e81ecbbe9
lassombra <briatt22@gmail.com>
2025-04-10 23:12:57 -0500

    Add new tab button

--- acdhandler.cpp
+++ acdhandler.cpp
@@ -2,6 +2,7 @@
 #include <qboxlayout.h>
 #include <qcoreapplication.h>
 #include <qdir.h>
+#include <qpushbutton.h>
 
 const QStringList groupHeaders = {"Name", "Time", "Deadline", "Relative Event", "Heading", "Automatic", "Manual", "Essential", "Sound", "Slow", "File"};
 const QStringList checklistHeaders = {"Text", "Time", "Relative Event", "Panel", "Heading 1", "Heading 2", "LF", "Info", "Item", "Position", "Automatic", "Call Group", "Fail Group", "Remarks"};
@@ -11,6 +12,20 @@ ACDHandler::ACDHandler(QWidget *parent)
 {
     currentFile = nullptr;
     this->setTabsClosable(true);
+    auto *newTabButton = new QPushButton("+");
+    QFontMetrics metrics(newTabButton->font());
+    int width = metrics.horizontalAdvance("+") + 6; // Add padding
+    int height = metrics.height() + 8; // Add padding
+    newTabButton->setFixedSize(width, height);
+
+    auto cornerWidget = new QWidget(this);
+    QHBoxLayout *cornerLayout = new QHBoxLayout(cornerWidget);
+    cornerLayout->setContentsMargins(0, 0, 0, 0); // Remove margins
+    cornerLayout->addWidget(newTabButton);
+    cornerLayout->addStretch(); // Ensure proper alignment without reserving extra space
+
+
+    this->setCornerWidget(cornerWidget);
     connect(this, &QTabWidget::tabCloseRequested, this, &ACDHandler::initRemoveTab);
     tabPool = new TabPool(this);
 }
